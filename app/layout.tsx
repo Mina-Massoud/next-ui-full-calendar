d2fd8e21bf74862f2dbcf9c16a49fde2e6a758b7
minamelad333 <mina@arabgiftcard.com>
2024-10-17 21:03:57 +0300

    add-switch-dependencie

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
+
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -8,8 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
-import { ModalProvider } from "@/providers/modal-provider";
-import { SchedulerProvider } from "@/providers/schedular-provider";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: {
@@ -43,25 +42,24 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-       
-          <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-            <Navbar />
-            <div className="relative flex flex-col h-screen">
-              <main className="w-full flex-grow">{children}</main>
-              <footer className="w-full flex items-center gap-3 justify-center py-3">
-                <span className="text-default-600">For Contacting</span>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <Navbar />
+          <div className="relative flex flex-col h-screen">
+            <main className="w-full flex-grow">{children}</main>
+            <footer className="w-full flex items-center gap-3 justify-center py-3">
+              <span className="text-default-600">For Contacting</span>
 
-                <Link
-                  isExternal
-                  className="flex items-center gap-1 text-current"
-                  href="https://mina-massoud.onrender.com/"
-                  title="nextui.org homepage"
-                >
-                  Mina Massoud
-                </Link>
-              </footer>
-            </div>
-          </Providers>
+              <Link
+                target="_blank"
+                className="flex items-center gap-1 text-current"
+                href="https://mina-massoud.onrender.com/"
+                title="nextui.org homepage"
+              >
+                Mina Massoud
+              </Link>
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
